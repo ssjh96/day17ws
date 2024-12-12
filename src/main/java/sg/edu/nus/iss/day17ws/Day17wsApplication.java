4daa01428aa34638f0b2f59766385a67087ab05d
ssjh96 <ssjh96@outlook.com>
2024-12-12 19:27:10 +0800

    added base

---
+++ src/main/java/sg/edu/nus/iss/day17ws/Day17wsApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.day17ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day17wsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day17wsApplication.class, args);
+	}
+
+}
---
+++ src/main/java/sg/edu/nus/iss/day17ws/constant/Constant.java
@@ -0,0 +1,11 @@
+package sg.edu.nus.iss.day17ws.constant;
+
+public class Constant {
+    
+    // Create constant for RedisConfig.java bean name
+    public static final String template01 = "template01";
+    public static final String template02 = "template02";
+
+    // Create key for redis repo
+    public static final String jokeKey = "jokes";
+}
